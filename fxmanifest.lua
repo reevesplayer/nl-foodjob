6a459014aee9c634ae0a9169be53ff288de4dc7d
Cryptic <reevesplayer@outlook.com>
2023-12-20 16:40:23 -0500

    Added files; created fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cryptic/Rip'
+description 'Food Delivery Job'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
+
+server_scripts {
+    'server/main.lua',
+    'server/version.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
